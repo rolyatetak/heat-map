dd13ff91cb3a40daf350a1c26f5072561c7542dd
kate taylor <35641557+rolyatetak@users.noreply.github.com>
2021-05-29 07:37:01 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,272 @@
+let data = [
+  {
+    "city": "London",
+    "country": "United Kingdom",
+    "months": [
+      5,
+      7,
+      9,
+      11,
+      14,
+      16,
+      19,
+      19,
+      17,
+      13,
+      10,
+      7
+    ]
+  },
+   {
+    "city": "Marrakesh",
+    "country": "Morocco",
+    "months": [
+        13,
+        14,
+        17,
+        18,
+        21,
+        24,
+        29,
+        29,
+        27,
+        21,
+        17,
+        14
+    ]
+},
+  {
+    "city": "Reykjavik",
+    "country": "Iceland",
+    "months": [
+      0,
+      0,
+      0,
+      3,
+      6,
+      9,
+      11,
+      11,
+      7,
+      4,
+      3,
+      2
+    ]
+  },
+ {
+    "city": "Copenhagen",
+   "country": "Denmark",
+    "months": [
+        0,
+        1,
+        3,
+        6,
+        12,
+        15,
+        17,
+        17,
+        14,
+        10,
+        5,
+        2
+    ]
+},
+ {
+    "city": "Mexico City",
+   "country": "Mexico",
+    "months": [
+        13,
+        14,
+        16,
+        18,
+        18,
+        18,
+        17,
+        18,
+        18,
+        17,
+        15,
+        13
+    ]
+},
+ {
+    "city": "Athens",
+    "country": "Greece",
+    "months": [
+        10,
+        10,
+        13,
+        16,
+        21,
+        26,
+        29,
+        29,
+        24,
+        20,
+        15,
+        12
+    ]
+},
+  {
+    "city": "Toronto",
+    "country": "Canada",
+    "months": [
+        -3,
+        -3,
+        1,
+        7,
+        14,
+        16,
+        21,
+        21,
+        18,
+        11,
+        5,
+        -1
+    ]
+},
+  {
+    "city": "Paris",
+    "country": "France",
+    "months": [
+        5,
+        6,
+        9,
+        11,
+        15,
+        16,
+        20,
+        20,
+        16,
+        12,
+        7,
+        5
+    ]
+},
+   {
+    "city": "Sydney",
+    "country": "Australia",
+    "months": [
+      23,
+      23,
+      22,
+      19,
+      16,
+      14,
+      13,
+      14,
+      16,
+      18,
+      20,
+      22
+    ]
+  },
+  {
+    "city": "Rome",
+    "country": "Italy",
+    "months": [
+        8,
+        9,
+        12,
+        14,
+        21,
+        23,
+        26,
+        24,
+        22,
+        18,
+        13,
+        10
+    ]
+},
+  {
+    "city": "Nuuk",
+    "country": "Greenland",
+    "months": [
+      -8,
+      -9,
+      -8,
+      -5,
+      2,
+      6,
+      8,
+      6,
+      4,
+      -1,
+      -4,
+      -6
+    ]
+  },
+  {
+    "city": "Dubai",
+    "country": "United Arab Emirates",
+    "months": [
+      19,
+      20,
+      23,
+      26,
+      31,
+      33,
+      35,
+      36,
+      33,
+      29,
+      25,
+      21
+    ]
+  },
+  {
+    "city": "Honolulu",
+    "country": "United States",
+    "months": [
+      23,
+      23,
+      24,
+      24,
+      25,
+      26,
+      26,
+      27,
+      27,
+      27,
+      25,
+      24
+    ]
+  },
+  {
+    "city": "Oslo",
+    "country": "Norway",
+    "months": [
+      -3,
+      -3,
+      2,
+      5,
+      12,
+      16,
+      18,
+      16,
+      12,
+      7,
+      2,
+      -3
+    ]
+  },
+  {
+    "city": "Auckland",
+		"country": "New Zealand",
+    "months": [
+      20,
+      20,
+      19,
+      17,
+      14,
+      12,
+      11,
+      12,
+      13,
+      15,
+      16,
+      18
+    ]
+  }
+]
